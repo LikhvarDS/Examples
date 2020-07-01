623888ee9362ed8c1a5f64191d6c95e40724f906
Лихвар Дмитрий (Likhvar_DS) <Likhvar_DS@directum.ru>
2020-07-01 10:58:23 +0400

    Конфигурация без проверки.

---
+++ 4.Reflection/Configuration/MyElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace Configuration
+{
+  public class MyElement : ConfigurationElement
+  {
+    [ConfigurationProperty(nameof(MySettingKey), IsRequired = true, IsKey = true)]
+    public string MySettingKey
+    {
+      get { return (string)this[nameof(this.MySettingKey)]; }
+    }
+
+    [ConfigurationProperty(nameof(MySettingValue), IsRequired = true)]
+    public int MySettingValue
+    {
+      get { return (int)this[nameof(this.MySettingValue)]; }
+    }
+  }
+}
