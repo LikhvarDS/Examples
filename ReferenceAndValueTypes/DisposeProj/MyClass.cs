53cc34197a1f0f59530b8717e59af89c4d6e8170
LDC7 <lihwar1@gmail.com>
2021-03-12 10:37:38 +0400

    fix after merge

---
+++ ReferenceAndValueTypes/DisposeProj/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DisposeProj
+{
+  class MyClass : IDisposable
+  {
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
