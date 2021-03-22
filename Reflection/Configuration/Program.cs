6a6eed1614c92afa2599f3af19b390ad6f35627e
LDC7 <lihwar1@gmail.com>
2021-03-22 10:01:05 +0400

    маленькие правки по reflection и dip

--- Reflection/Configuration/Program.cs
+++ Reflection/Configuration/Program.cs
@@ -7,13 +7,16 @@ namespace Configuration
   {
     public static void Main()
     {
-      foreach(var key in ConfigurationManager.AppSettings.AllKeys)
-      {
+      foreach (var key in ConfigurationManager.AppSettings.AllKeys)
         Console.WriteLine(key);
-      }
+
       Console.ReadKey();
 
+
       var settingsSection = (MySection)ConfigurationManager.GetSection("MySettings");
+      foreach (MyElement setting in settingsSection.MySettings)
+        Console.WriteLine($"{setting.MySettingKey}: {setting.MySettingValue}");
+
       Console.ReadKey();
     }
   }
